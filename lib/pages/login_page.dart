9218fd6abcf571a0fca9c45c070d2697df709bd7
Kemasjava9 <javakemaspackaging@gmail.com>
2025-11-29 10:45:06 +0700

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -153,7 +153,7 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Future<void> _downloadApp() async {
-    const url = 'output/app-realese.apk';
+    const url = 'https://github.com/Kemasjava9/kasirweb/raw/gh-pages/app-release.apk';
     final uri = Uri.parse(url);
     if (await canLaunchUrl(uri)) {
       await launchUrl(uri);
@@ -381,4 +381,5 @@ class _LoginPageState extends State<LoginPage> {
     _passwordController.dispose();
     super.dispose();
   }
+
 }
