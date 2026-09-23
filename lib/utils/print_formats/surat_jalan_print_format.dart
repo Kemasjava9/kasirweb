db4a895e9033163e7b7339579a34f4919c0bf7c4
Kemasjava9 <javakemaspackaging@gmail.com>
2026-09-24 04:52:43 +0700

    Refactor SuratJalanPrintFormat for PDF generation

--- lib/utils/print_formats/surat_jalan_print_format.dart
+++ lib/utils/print_formats/surat_jalan_print_format.dart
@@ -1,5 +1,7 @@
 import 'dart:typed_data';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+
 
 class SuratJalanPrintFormat {
   static Future<Uint8List> buildSuratJalanPdf(Map<String, dynamic> data) async {
